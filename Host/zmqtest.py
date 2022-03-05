fb67fc883541df4e6f355790cc26a20e75c9b40d
joman301 <jbvondracek@crimson.ua.edu>
2022-03-04 18:38:02 -0600

    one last push for the evening - need to start building a push/pull threading

--- Host/zmqtest.py
+++ Host/zmqtest.py
@@ -1,11 +1,23 @@
 import zmq
+import queue
 
 context = zmq.Context()
 
 #  Socket to talk to server
 print("Connecting to hello world server…")
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://192.168.7.2:5555")
+context = zmq.Context()
+receive_socket = context.socket(zmq.PULL)
+receive_socket.connect("tcp://192.168.7.2:5555")
+send_socket = context.socket(zmq.PUSH)
+send_socket.connect("tcp://192.168.7.2:5556")
+
+# Queue of all data that will later be sent to the server
+SEND_INFO = queue.Queue()
+
+# Queue of all demands from the server
+RECEIVED_LOGS = queue.Queue()
+RECEIVED_MESSAGES = queue.Queue()
+
 
 #  Do 10 requests, waiting each time for a response
 for request in range(10):
