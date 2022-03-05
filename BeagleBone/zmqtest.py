c308fa29e8d63d5f6edb7aac059fa263fb04eba9
joman301 <jbvondracek@crimson.ua.edu>
2022-03-04 18:00:43 -0600

    zmq pleaseeeeeeeeeee

--- BeagleBone/zmqtest.py
+++ BeagleBone/zmqtest.py
@@ -3,7 +3,7 @@ import zmq
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
-socket.bind("tcp://192.168.7.3:5555")
+socket.bind("tcp://192.168.7.2:5555")
 
 while True:
     #  Wait for next request from client
--- Host/zmqtest.py
+++ Host/zmqtest.py
@@ -5,7 +5,7 @@ context = zmq.Context()
 #  Socket to talk to server
 print("Connecting to hello world server…")
 socket = context.socket(zmq.REQ)
-socket.connect("tcp://192.168.7.1:5555")
+socket.connect("tcp://192.168.7.3:5555")
 
 #  Do 10 requests, waiting each time for a response
 for request in range(10):
