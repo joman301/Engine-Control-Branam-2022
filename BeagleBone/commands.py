f3fd761949aa780c12c63dd160ee0503743309d3
joman301 <jbvondracek@crimson.ua.edu>
2022-03-11 14:55:26 -0600

    please

--- BeagleBone/commands.py
+++ BeagleBone/commands.py
@@ -134,8 +134,9 @@ def log(currently_logging):
 
 def pt_simulation(currently_generating):
     if currently_generating == "on":    
-        msg.tell("SIMULATION OPTIONS:\n1.) Linear\n 2.) Exponential\n 3.) Random\n(Put random character to quit)")
+        msg.tell("SIMULATION OPTIONS:\n1.) Linear\n2.) Exponential\n3.) Random\n(Put random character to quit)")
         type = msg.demand("Enter the simulation type's number:")
+        msg.tell("You entered {}".format(type))
         if type != 1: # not(type == 1 or type == 2 or type == 3):
             msg.tell("Quitting the simulator")
             return
