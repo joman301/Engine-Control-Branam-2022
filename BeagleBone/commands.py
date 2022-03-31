b64e7a51ee01f47ba1ae107177a4f7e7c451634b
joman301 <jbvondracek@crimson.ua.edu>
2022-03-31 16:14:32 -0500

    i think i gotcha now you son of a gun

--- BeagleBone/commands.py
+++ BeagleBone/commands.py
@@ -363,12 +363,8 @@ def pt_check():
 def sys():
     global STATES
     states_string = json.dumps(STATES)
-    for letter in states_string:
-        if letter == ',':    
-            states_string = states_string[:1] + "\n" + states_string[1:]
-
     print("Printed System States")
-    msg.tell(states_string)
+    msg.tell(states_string.replace(",", "\n"))
 
 def quit():
     print("Cleaning the pins of the BBB")
