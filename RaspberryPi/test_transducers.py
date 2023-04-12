a92c92918412c426bba16155a6cdea88de6073d4
joman301 <joman301@gmail.com>
2023-04-12 13:45:46 -0500

    Updating the test to include more sensors

--- RaspberryPi/test_transducers.py
+++ RaspberryPi/test_transducers.py
@@ -12,5 +12,8 @@ adc.set_pga(ADC_GAIN)
 
 sleep(1)
 while True:
-    print(adc.read_voltage(1))
+    print("PT 5: " + str(adc.read_voltage(5)))
+    print("PT 6: " + str(adc.read_voltage(6)))
+    print("PT 7: " + str(adc.read_voltage(7)))
+    print("PT 8: " + str(adc.read_voltage(8) + "\n"))
     sleep(0.1)
